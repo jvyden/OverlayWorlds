7be75e6209ecca4cfdd86781a9068088d6148871
jvyden <jvyden@jvyden.xyz>
2025-05-14 15:58:16 -0400

    i mean, it KINDA works

--- OverlayWorlds/BasicPatcher.cs
+++ OverlayWorlds/BasicPatcher.cs
@@ -1,8 +1,11 @@
-﻿using FrooxEngine.ProtoFlux;
+﻿using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
 using HarmonyLib;
 using MonkeyLoader.Patching;
 using MonkeyLoader.Resonite;
 using MonkeyLoader.Resonite.Features.FrooxEngine;
+using SkyFrost.Base;
 
 namespace OverlayWorlds
 {
@@ -16,9 +19,29 @@ namespace OverlayWorlds
             yield return new FeaturePatch<ProtofluxTool>(PatchCompatibility.HookOnly);
         }
 
+        private static void SetupWorld(World w)
+        {
+            Slot ground = w.AddSlot("Ground");
+            ground.AttachCylinder<PBS_Metallic>(15f, 0.2f).Sides.Value = 128;
+            ground.GetComponent<CylinderCollider>().SetCharacterCollider();
+            ground.GlobalPosition = float3.Down * 0.1f;
+        }
+
         private static void Postfix()
         {
             Logger.Info(() => "Postfix for ProtoFluxTool.OnAttach()!");
+            Task.Run(async () =>
+            {
+                World world = await Userspace.OpenWorld(new WorldStartSettings
+                {
+                    InitWorld = SetupWorld,
+                    AutoFocus = false,
+                    HideFromListing = true,
+                    GetExisting = false,
+                    DefaultAccessLevel = SessionAccessLevel.Private,
+                });
+                world.Focus = World.WorldFocus.Overlay;
+            });
         }
     }
 }
