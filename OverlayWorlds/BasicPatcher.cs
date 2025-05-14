32930a5171808bcb4b8c4cf3fcfb2578e5a071ac
jvyden <jvyden@jvyden.xyz>
2025-05-14 16:12:16 -0400

    it works!

--- OverlayWorlds/BasicPatcher.cs
+++ OverlayWorlds/BasicPatcher.cs
@@ -7,41 +7,43 @@ using MonkeyLoader.Resonite;
 using MonkeyLoader.Resonite.Features.FrooxEngine;
 using SkyFrost.Base;
 
-namespace OverlayWorlds
+namespace OverlayWorlds;
+
+[HarmonyPatchCategory(nameof(BasicPatcher))]
+[HarmonyPatch(typeof(ProtoFluxTool), nameof(ProtoFluxTool.OnAttach))]
+internal class BasicPatcher : ResoniteMonkey<BasicPatcher>
 {
-    [HarmonyPatchCategory(nameof(BasicPatcher))]
-    [HarmonyPatch(typeof(ProtoFluxTool), nameof(ProtoFluxTool.OnAttach))]
-    internal class BasicPatcher : ResoniteMonkey<BasicPatcher>
+    // The options for these should be provided by your game's game pack.
+    protected override IEnumerable<IFeaturePatch> GetFeaturePatches()
     {
-        // The options for these should be provided by your game's game pack.
-        protected override IEnumerable<IFeaturePatch> GetFeaturePatches()
-        {
-            yield return new FeaturePatch<ProtofluxTool>(PatchCompatibility.HookOnly);
-        }
+        yield return new FeaturePatch<ProtofluxTool>(PatchCompatibility.HookOnly);
+    }
 
-        private static void SetupWorld(World w)
-        {
-            Slot ground = w.AddSlot("Ground");
-            ground.AttachCylinder<PBS_Metallic>(15f, 0.2f).Sides.Value = 128;
-            ground.GetComponent<CylinderCollider>().SetCharacterCollider();
-            ground.GlobalPosition = float3.Down * 0.1f;
-        }
+    private static void SetupWorld(World w)
+    {
+        Slot ground = w.AddSlot("Ground");
+        ground.AttachCylinder<PBS_Metallic>(15f, 0.2f).Sides.Value = 128;
+        ground.GetComponent<CylinderCollider>().SetCharacterCollider();
+        ground.GlobalPosition = float3.Down * 0.1f;
+    }
 
-        private static void Postfix()
+    private static void Postfix()
+    {
+        Logger.Info(() => "Postfix for ProtoFluxTool.OnAttach()!");
+        Task.Run(async () =>
         {
-            Logger.Info(() => "Postfix for ProtoFluxTool.OnAttach()!");
-            Task.Run(async () =>
+            World world = await Userspace.OpenWorld(new WorldStartSettings
             {
-                World world = await Userspace.OpenWorld(new WorldStartSettings
-                {
-                    InitWorld = SetupWorld,
-                    AutoFocus = false,
-                    HideFromListing = true,
-                    GetExisting = false,
-                    DefaultAccessLevel = SessionAccessLevel.Private,
-                });
-                world.Focus = World.WorldFocus.Overlay;
+                InitWorld = SetupWorld,
+                AutoFocus = false,
+                HideFromListing = true,
+                GetExisting = false,
+                DefaultAccessLevel = SessionAccessLevel.Private,
+                FetchedWorldName = "Pocket Dimension",
+                CreateLoadIndicator = false,
             });
-        }
+            world.Focus = World.WorldFocus.Overlay;
+            world.Name = "Pocket Dimension";
+        });
     }
 }
